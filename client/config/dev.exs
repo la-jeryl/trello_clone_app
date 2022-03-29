f469b07c6822803d55969bbd44b6881516787df9
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-03-29 17:17:33 +0800

    add tailwind css

--- client/config/dev.exs
+++ client/config/dev.exs
@@ -25,7 +25,8 @@ config :client, ClientWeb.Endpoint,
   secret_key_base: "9pvnrHdMNU4roDDCDvAAflYE27drfKvSEd4OUq22gaPpVdl/4vaqj5wxHyGTGdsE",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
