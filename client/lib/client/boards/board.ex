f3f41591e9246aa5e3beabdb6f717824d4b42a34
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-03-30 15:10:07 +0800

    updated the board and user schema

--- client/lib/client/boards/board.ex
+++ client/lib/client/boards/board.ex
@@ -2,9 +2,6 @@ defmodule Client.Boards.Board do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias Client.Users.User
-  alias Client.Lists.List
-
   schema "boards" do
     field :title, :string, null: false
   end
--- client/lib/client/users/user.ex
+++ client/lib/client/users/user.ex
@@ -18,4 +18,30 @@ defmodule Client.Users.User do
     |> cast(attrs, [:email, :password, :confirm_password, :role])
     |> validate_required([:email, :password])
   end
+
+  def change_user_registration(%__MODULE__{} = user, attrs \\ %{}) do
+    __MODULE__.registration_changeset(user, attrs, hash_password: false)
+  end
+
+  def registration_changeset(user, attrs, opts \\ []) do
+    user
+    |> cast(attrs, [:email, :password])
+    |> validate_email()
+    |> validate_password(opts)
+  end
+
+  defp validate_email(changeset) do
+    changeset
+    |> validate_required([:email])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> validate_length(:email, max: 160)
+    |> unsafe_validate_unique(:email, Client.Repo)
+    |> unique_constraint(:email)
+  end
+
+  defp validate_password(changeset, _opts) do
+    changeset
+    |> validate_required([:password])
+    |> validate_length(:password, min: 5, max: 72)
+  end
 end
