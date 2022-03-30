7fd46a626461090de64bf8ba14446a80bf8db1f1
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-03-30 15:29:34 +0800

    minor update on user changeset

--- client/lib/client/users/user.ex
+++ client/lib/client/users/user.ex
@@ -33,7 +33,9 @@ defmodule Client.Users.User do
   defp validate_email(changeset) do
     changeset
     |> validate_required([:email])
-    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/,
+      message: "Email must have the @ sign and no spaces"
+    )
     |> validate_length(:email, max: 160)
     |> unsafe_validate_unique(:email, Client.Repo)
     |> unique_constraint(:email)
