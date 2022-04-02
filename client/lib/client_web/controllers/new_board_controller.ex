e7d5c9c324d6284941775b1051c83b956bec3375
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-04-02 21:47:22 +0800

    added a comment in case put_flash is preferred

--- client/lib/client_web/controllers/new_board_controller.ex
+++ client/lib/client_web/controllers/new_board_controller.ex
@@ -40,6 +40,7 @@ defmodule ClientWeb.NewBoardController do
         conn
         |> put_session(:boards, initial_boards_list)
         |> put_session(:board_id, initial_board_id)
+        # |> put_flash(:error, reason["error"])
         |> render("new_board.html", error_message: reason["error"], boards: initial_boards_list)
     end
   end
